c6c34337ca7ff91f5d586c7ba6c55a35d3dd6d67
johnsonr <johnsonroda@gmail.com>
2016-03-05 17:06:19 +1100

    Delete params.js

--- whatever/template1/scripts/params.js
+++
@@ -1,6 +0,0 @@
-
-// The very important parameter that is the project name reversed
-function computed_parameters(m) {
-  var reversed = m["name"].reverse();
-  return { reversed_name: reversed };
-}
