d446afbe45518a50183c6c88b62b934a39241daf
johnsonr <johnsonroda@gmail.com>
2016-03-05 17:06:38 +1100

    Create params.js

---
+++ whatever/template1/meta/params.js
@@ -0,0 +1,6 @@
+
+// The very important parameter that is the project name reversed
+function computed_parameters(m) {
+  var reversed = m["name"].reverse();
+  return { reversed_name: reversed };
+}
