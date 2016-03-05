4b16382c5f1769c9ef3dfd777a5d05280694d82d
johnsonr <johnsonroda@gmail.com>
2016-03-05 17:13:05 +1100

    Update params.js

--- whatever/template1/meta/params.js
+++ whatever/template1/meta/params.js
@@ -1,6 +1,7 @@
 
 // The very important parameter that is the project name reversed
 function computed_parameters(m) {
-  var reversed = m["name"].reverse();
+  var name = m["name"];
+  var reversed = name.split("").reverse().join("");
   return { reversed_name: reversed };
 }
