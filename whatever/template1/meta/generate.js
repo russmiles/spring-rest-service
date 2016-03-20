b398a47001666a9e5ecc03419fe2f6b88d114be4
johnsonr <johnsonroda@gmail.com>
2016-03-21 10:39:21 +1100

    Moved files into root

--- whatever/template1/meta/generate.js
+++
@@ -1,5 +0,0 @@
-
-// Generated
-function generate(m) {
-  return new GeneratedFile(m["name"] + ".txt", m["name"]);
-}
--- whatever/template1/meta/params.js
+++
@@ -1,7 +0,0 @@
-
-// The very important parameter that is the project name reversed
-function computed_parameters(m) {
-  var name = m["name"];
-  var reversed = name.split("").reverse().join("");
-  return { reversed_name: reversed };
-}
